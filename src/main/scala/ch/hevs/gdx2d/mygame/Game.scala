8158b107f258defeec2f0e91f038dc4f4198e184
AmineF-collab <ferchichiamine2007@gmail.com>
2026-05-28 12:46:29 +0200

    mise au propre des classes dans un fichier propre

---
+++ src/main/scala/ch/hevs/gdx2d/mygame/Game.scala
@@ -0,0 +1,74 @@
+package ch.hevs.gdx2d.mygame
+
+import ch.hevs.gdx2d.components.physics.primitives.PhysicsStaticBox
+import ch.hevs.gdx2d.components.physics.utils.PhysicsScreenBoundaries
+import ch.hevs.gdx2d.desktop.DesktopApplication
+import ch.hevs.gdx2d.lib.GdxGraphics
+import ch.hevs.gdx2d.lib.physics.PhysicsWorld
+import com.badlogic.gdx.{Gdx, Input}
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.World
+
+import scala.collection.mutable.ArrayBuffer
+
+class Game(var number_player: Int, var map_name: String) extends DesktopApplication(1920, 1080) {
+
+  private var world: World = _
+  val assets: GameAssets = new GameAssets
+  val mapsManager: MapsManager = new MapsManager
+  private var c1: T2DCar = _
+  private var hitboxes: ArrayBuffer[PhysicsStaticBox] = _
+  var zoom = 2f
+
+
+  override def onInit(): Unit = {
+    setTitle("RACE")
+
+    assets.loadAll()
+    assets.manager.finishLoading()
+
+    world = PhysicsWorld.getInstance()
+    world.setGravity(new Vector2(0f, 0f))
+
+    val loadedMap = assets.getMap()
+    mapsManager.load(loadedMap)
+    hitboxes = assets.generateHitBoxes() // résultat stocké
+    new PhysicsScreenBoundaries(getWindowWidth.toFloat, getWindowHeight.toFloat)
+
+    c1 = new T2DCar(new Vector2(assets.spawnPlacementForTheCar()(0)))
+  }
+
+  override def onGraphicRender(g: GdxGraphics): Unit = {
+    g.clear()
+    PhysicsWorld.updatePhysics(Gdx.graphics.getDeltaTime)
+
+    c1.draw(g)
+
+    val camera: OrthographicCamera = g.getCamera
+    g.moveCamera(c1.box.getBodyPosition.x - 1920 / 2 * zoom, c1.box.getBodyPosition.y - 1080 / 2 * zoom)
+    g.zoom(zoom)
+    camera.update()
+
+    mapsManager.render(camera)
+
+  }
+
+  override def onKeyUp(keycode: Int): Unit = {
+    keycode match {
+      case Input.Keys.LEFT => c1.driftLeft = false
+      case Input.Keys.RIGHT => c1.driftRight = false
+      case Input.Keys.UP => c1.driveUp = 0f
+      case _ => ()
+    }
+  }
+
+  override def onKeyDown(keycode: Int): Unit = {
+    keycode match {
+      case Input.Keys.LEFT => c1.driftLeft = true
+      case Input.Keys.RIGHT => c1.driftRight = true
+      case Input.Keys.UP => c1.driveUp = T2DCar.MAX_THRUST
+      case _ => ()
+    }
+  }
+}
--- src/main/scala/ch/hevs/gdx2d/mygame/Menu.scala
+++ src/main/scala/ch/hevs/gdx2d/mygame/Menu.scala
@@ -9,14 +9,12 @@ import ch.hevs.gdx2d.desktop.physics.DebugRenderer
 import ch.hevs.gdx2d.lib.GdxGraphics
 import ch.hevs.gdx2d.lib.physics.PhysicsWorld
 import ch.hevs.gdx2d.lib.utils.Logger
-import ch.hevs.gdx2d.mygame.GameAssets
+import ch.hevs.gdx2d.mygame.{Game, GameAssets, MapsManager, T2DCar}
 import com.badlogic.gdx.Input.Keys
-import ch.hevs.gdx2d.mygame.T2DCar
 import com.badlogic.gdx.{Gdx, Input}
 import com.badlogic.gdx.graphics.{Color, OrthographicCamera, Texture}
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.World
-import ch.hevs.gdx2d.mygame.MapsManager
 import com.badlogic.gdx.scenes.scene2d.{InputEvent, Stage}
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox.SelectBoxStyle
 import com.badlogic.gdx.scenes.scene2d.ui.{Label, SelectBox, Skin, TextButton}
@@ -25,66 +23,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import scala.collection.SeqView.DropRight
 import scala.collection.mutable.ArrayBuffer
 
-class Game(var number_player: Int, var map_name: String) extends DesktopApplication(1920, 1080) {
 
-  private var world: World = _
-  val assets: GameAssets = new GameAssets
-  val mapsManager: MapsManager = new MapsManager
-  private var c1: T2DCar = _
-  private var hitboxes: ArrayBuffer[PhysicsStaticBox] = _
-  var zoom = 2f
-
-
-  override def onInit(): Unit = {
-    setTitle("RACE")
-
-    assets.loadAll()
-    assets.manager.finishLoading()
-
-    world = PhysicsWorld.getInstance()
-    world.setGravity(new Vector2(0f, 0f))
-
-    val loadedMap = assets.getMap()
-    mapsManager.load(loadedMap)
-    hitboxes = assets.generateHitBoxes() // résultat stocké
-    new PhysicsScreenBoundaries(getWindowWidth.toFloat, getWindowHeight.toFloat)
-
-    c1 = new T2DCar(new Vector2(assets.spawnPlacementForTheCar()(0)))
-  }
-
-  override def onGraphicRender(g: GdxGraphics): Unit = {
-    g.clear()
-    PhysicsWorld.updatePhysics(Gdx.graphics.getDeltaTime)
-
-    c1.draw(g)
-
-    val camera: OrthographicCamera = g.getCamera
-    g.moveCamera(c1.box.getBodyPosition.x - 1920 / 2 * zoom, c1.box.getBodyPosition.y - 1080 / 2 * zoom)
-    g.zoom(zoom)
-    camera.update()
-
-    mapsManager.render(camera)
-
-  }
-
-  override def onKeyUp(keycode: Int): Unit = {
-    keycode match {
-      case Input.Keys.LEFT => c1.driftLeft = false
-      case Input.Keys.RIGHT => c1.driftRight = false
-      case Input.Keys.UP => c1.driveUp = 0f
-      case _ => ()
-    }
-  }
-
-  override def onKeyDown(keycode: Int): Unit = {
-    keycode match {
-      case Input.Keys.LEFT => c1.driftLeft = true
-      case Input.Keys.RIGHT => c1.driftRight = true
-      case Input.Keys.UP => c1.driveUp = T2DCar.MAX_THRUST
-      case _ => ()
-    }
-  }
-}
 
 class Menu() extends DesktopApplication(1920, 1080) {
   private var stage: Stage = _
@@ -195,7 +134,7 @@ class Menu() extends DesktopApplication(1920, 1080) {
 
 }
 
-object Launch {
+object Menu {
   def main(args: Array[String]): Unit = {
     new Menu().launch()
 
